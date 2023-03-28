2094271e8661121a455692dc825340e0bec1dfab
Damianopipitone <damianopipitone33@gmail.com>
2023-03-28 15:06:38 +0200

    Password generator

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const name = prompt ("Inserisci il tuo nome...");
+
+let age = prompt ("Inserisci la tua età...");
+
+const color = prompt ("Inserisci il tuo colore preferito...");
+
+console.log ( 
+    `
+    ${name} ${age} ${color}
+`
+);
