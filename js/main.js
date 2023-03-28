34fffb7b11465eec3d6f1a883c9704d699b68b83
Damianopipitone <damianopipitone33@gmail.com>
2023-03-28 15:22:36 +0200

    Password Generator

--- js/main.js
+++ js/main.js
@@ -4,8 +4,16 @@ let age = prompt ("Inserisci la tua età...");
 
 const color = prompt ("Inserisci il tuo colore preferito...");
 
+let number1 = prompt ("Inserisci un numero... ");
+
+let number2 = prompt ("Inserisci un altro numero...")
+
+let division = number1 / number2;
+
 console.log ( 
-    `
-    ${name} ${age} ${color}
+`
+    ${name}${age}${color}${division}
 `
 );
+
+document.getElementById ("password") = ""
